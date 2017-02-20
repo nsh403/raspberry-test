3e9d6ce50f4dab68e7d693f001bb51ed0247dc6c
nash <nash403@gmail.com>
2017-02-20 21:08:15 +0900

    temp

--- matplotlib-copy/1-simple_anim.py
+++ matplotlib-copy/1-simple_anim.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
-print fig
-print ax
+print (fig)
+print (ax)
 
 x = np.arange(0, 2*np.pi, 0.01)        # x-array
 line, = ax.plot(x, np.sin(x))
 #print x
-print line
+print (line)
 
 def animate(i):
     line.set_ydata(np.sin(x+i/10.0))  # update the data
